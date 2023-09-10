716ef4e0b7a13b6890971f519040e2c8af20653a
tikendraw <tikendraksahu1029@gmail.com>
2023-09-10 15:46:21 +0530

    predict

--- predict.py
+++ predict.py
@@ -1,18 +1,27 @@
-# predict.py
 import argparse
+import logging
 import os
+from datetime import datetime
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
 import torch
-from funcyou.utils import DotDict, set_seed
+from funcyou.utils import DotDict
 from PIL import Image
+from sklearn.metrics import (accuracy_score, f1_score, precision_score,
+                             recall_score)
 from torch.utils.data import DataLoader
 from torchvision import transforms
+from torchvision.datasets import ImageFolder
 from tqdm import tqdm
 
-from data import ImageFolderWithPaths
 from model import VisionTransformer
+from utils import evaluate_model, set_seed
+
+# Configure logger
+logging.basicConfig(filename='prediction.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+logger = logging.getLogger()
 
 
 def load_and_preprocess_image(image_path, transform):
@@ -23,36 +32,65 @@ def load_and_preprocess_image(image_path, transform):
 def predict_directory(directory_path, model, transform, batch_size=8, device='cpu'):
     model.eval()
 
-    image_paths = [os.path.join(directory_path, filename) for filename in os.listdir(directory_path)]
-
-    dataset = ImageFolderWithPaths(directory_path, transform=transform)
+    
+    dataset = ImageFolder(directory_path, transform=transform)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=2)
 
+    image_paths = [i[0] for i in dataset.imgs]
+
     predictions = []
-    confidence_scores = []
-    image_paths_list = []
 
     with torch.no_grad():
-        for images, _, paths in tqdm(dataloader, desc="Predicting"):
+        for images, labels in tqdm(dataloader, desc="Predicting"):
             images = images.to(device)
-            outputs, _ = model(images)
-            outputs = torch.sigmoid(outputs).cpu().numpy()
+            outputs,_ = model(images)
+            predicted = torch.round(outputs.squeeze())
+            
+            predictions.extend(predicted.cpu().numpy())
 
-            predictions.extend(np.round(outputs))
-            confidence_scores.extend(outputs.max(axis=1))
-            image_paths_list.extend(paths)
+    print('image paths: ', len(image_paths))
+    print('pred shap: ', len(predictions))
+    return pd.DataFrame(
+        {
+            "Image_Path": image_paths,
+            "Predicted_Class": predictions,
+        }
+    )
+
+def test_directory(directory_path, model, transform, batch_size=8, device='cpu'):
+    model.eval()
+    directory_path = Path(directory_path)
+
+    dataset = ImageFolder(directory_path, transform=transform)
+    dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=2)
+
+    y_true = []
+    y_pred = []
+
+    with torch.no_grad():
+        for images, labels in tqdm(dataloader, desc="Predicting"):
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs,_ = model(images)
+            predicted = torch.round(outputs.squeeze())
+            
+            y_true.extend(labels.cpu().numpy())
+            y_pred.extend(predicted.cpu().numpy())
 
     return pd.DataFrame(
         {
-            "Image_Path": image_paths_list,
-            "Predicted_Class": predictions,
-            "Confidence_Score": confidence_scores,
+            "Image_Path": dataset.imgs,
+            "True_Class": y_true,
+            "Predicted_Class": y_pred,
         }
     )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Pneumonia Detection using Vision Transformer')
     parser.add_argument('image_directory', type=str, help='Path to the directory containing test folder that contains images')
+    parser.add_argument('--test', action='store_true', default=False, help='flags to test a folder')
+
     args = parser.parse_args()
 
     config = DotDict.from_toml('config.toml')
@@ -73,9 +111,44 @@ if __name__ == "__main__":
     ])
     
     image_directory = Path(args.image_directory) 
+    test = args.test
     
-    result_df = predict_directory(image_directory, model, test_transform)
+    logger.info(f'Testing directory: {image_directory}')
     
+    if test:
+        result_df = test_directory(image_directory, model, test_transform, batch_size=8, device=config.device)
+        
+        # Calculate evaluation metrics
+        y_true = result_df["True_Class"].values
+        y_pred = result_df["Predicted_Class"].values
+        accuracy = accuracy_score(y_true, y_pred)
+        precision = precision_score(y_true, y_pred)
+        recall = recall_score(y_true, y_pred)
+        f1 = f1_score(y_true, y_pred)
+        
+        # Log the metrics
+        logger.info(f'Accuracy: {accuracy}')
+        logger.info(f'Precision: {precision}')
+        logger.info(f'Recall: {recall}')
+        logger.info(f'F1 Score: {f1}')
+        
+        # Print metrics in the terminal
+        print(f'Accuracy: {accuracy}')
+        print(f'Precision: {precision}')
+        print(f'Recall: {recall}')
+        print(f'F1 Score: {f1}')
+    else:
+        result_df = predict_directory(image_directory, model, test_transform)
+    
+    # Log the number of images
+    num_images = len(result_df)
+    logger.info(f'Number of images: {num_images}')
+    
+    ttime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    ttime = str(ttime).replace('/','-')
+    filename = f'output-{ttime}.csv'
     print(result_df)  # Display the first few rows of the DataFrame
-    result_df.to_csv('output.csv')
-    print('Output saved in output.csv!')
+    result_df.to_csv(filename)
+    print(f'Output saved in {filename}!')
+    logger.info(f'Output saved in {filename}!')
+    logger.info('-'*40)
