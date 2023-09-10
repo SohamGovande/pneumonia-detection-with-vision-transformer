92e5f8c84102fdd03d5c4baa75b4cf8735577c1b
tikendraw <tikendraksahu1029@gmail.com>
2023-09-10 22:25:20 +0530

    changed load_image_data func

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, WeightedRandomSampler
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from torchvision.datasets.folder import default_loader
@@ -10,53 +10,29 @@ from torchvision.datasets.folder import default_loader
 from utils import calculate_class_weights_from_directory
 
 
-def load_data(data_dir, image_size, batch_size, num_workers, test=False):
-    # Define data augmentation transformations for X-ray images
+def load_image_data(data_dir:Path, image_size, batch_size=1, num_workers=2, transform=None, use_sampler=False, shuffle=False):
+    if transform is None:
+        transform = transforms.Compose([
+            transforms.Resize((image_size, image_size)),
+            transforms.ToTensor(),
+        ])
 
-    train_dir = os.path.join(data_dir, 'train')
-    test_dir = os.path.join(data_dir, 'test')
+    # Load data directly from the directory
+    dataset = ImageFolder(root=data_dir, transform=transform)
 
-    train_transform = transforms.Compose([
-        transforms.RandomRotation(degrees=(-10, 10)),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomResizedCrop(image_size, scale=(0.8, 1.2)),
-        transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
-        transforms.ToTensor(), 
-    ])
 
-    test_transform = transforms.Compose([
-        transforms.Resize((image_size,image_size)),
-        transforms.ToTensor()
-    ])
-
-    # Train data
-    train_dataset = ImageFolder(root=train_dir, transform=train_transform)
-
-    class_dist, class_weights, bincount = calculate_class_weights_from_directory(train_dir)
-    train_sampler = torch.utils.data.WeightedRandomSampler(class_weights, len(class_weights), replacement=True)
-
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler = train_sampler, num_workers=2)
-
-    if test:
-        # Test data
-        test_dataset = ImageFolder(root=test_dir, transform=test_transform)
-        test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-
-        return train_dataloader, test_dataloader
-    else:
-        return train_dataloader
+    if not use_sampler:
+        return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
     
-    
-class ImageFolderWithPaths(ImageFolder):
-    def __init__(self, root, transform=None, target_transform=None, loader=default_loader):
-        super(ImageFolderWithPaths, self).__init__(root, transform, target_transform, loader)
-
-    def __getitem__(self, index):
-        original_tuple = super(ImageFolderWithPaths, self).__getitem__(index)
-        path, _ = self.samples[index]
-        return (original_tuple + (path,))
-
-
+    _, class_weights, _ = calculate_class_weights_from_directory(data_dir)
+    sampler = WeightedRandomSampler(class_weights, len(class_weights), replacement=True)
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        sampler=sampler,
+        num_workers=num_workers,
+    )
+         
 
 if __name__ == "__main__":
     print('hello world!')
